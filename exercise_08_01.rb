aeeadee002c65b6bb9ab5eef10f568558e87fa99
ongaeshi <ongaeshi0621@gmail.com>
2017-11-19 16:47:20 +0900

    Add line recursive flactal

---
+++ exercise_08_01.rb
@@ -0,0 +1,26 @@
+Graphics.set_background(Palette::White)
+
+def draw_lines(x1, y1, x2, y2)
+  Line.new(x1, y1, x2, y2).draw(1, Palette::Black)
+
+  dx = x2 - x1
+  dy = y2 - y1
+
+  if dx == 0 && dy > 4
+    draw_lines(x1 - dy/3, y1, x1 + dy/3, y1)
+    draw_lines(x1 - dy/3, y2, x1 + dy/3, y2)
+  elsif dy == 0 && dx > 4
+    draw_lines(x1, y1 - dx/3, x1, y1 + dx/3)
+    draw_lines(x2, y1 - dx/3, x2, y2 + dx/3)
+  end
+end
+
+camera = Camera2D.new
+
+while System.update do
+  camera.update
+  camera.draw(Palette::Orange)
+  camera.transform do
+    draw_lines(100, 100, 700, 100)
+  end
+end
